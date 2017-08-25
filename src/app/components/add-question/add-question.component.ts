8047a402aa1b807fa11f3e3980ba4853044a97f5
Scott Turnbull <streamweaver@gmail.com>
2017-08-25 13:26:16 -0400

    Added component to add question, function not complete

---
+++ src/app/components/add-question/add-question.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Question } from '../../models/question';
+
+@Component({
+  selector: 'app-add-question',
+  templateUrl: './add-question.component.html',
+  styleUrls: ['./add-question.component.scss']
+})
+export class AddQuestionComponent implements OnInit {
+  @Output() questionAdded = new EventEmitter<Question>();
+  q: string;
+  a: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addQuestion() {
+    this.questionAdded.emit({q: this.q, a: this.a, hide: true});
+  }
+
+}
