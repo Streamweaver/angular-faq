e7afcf6aa179b6c07a8874c22bdf2c740b4039ad
Scott Turnbull <streamweaver@gmail.com>
2017-08-25 12:57:20 -0400

    Moved data out into a basic dataservice

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Question } from '../models/question';
+
+@Injectable()
+export class DataService {
+  questions: Question[];
+
+  constructor() {
+    this.questions = [
+      {
+        q: 'What is your name?',
+        a: 'My name is Scott',
+        hide: true
+      },
+      {
+        q: 'What is your favorite color?',
+        a: 'My favorite color is blue',
+        hide: true
+      },
+      {
+        q: 'What is your favorite programming language?',
+        a: 'My favorite programming language is python or Go',
+        hide: true
+      }
+    ];
+  }
+
+  getQuestions() {
+    return this.questions;
+  }
+}
