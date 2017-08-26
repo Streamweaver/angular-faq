5b3576f750aad42d98db9caefdf5f5483705df3b
Scott Turnbull <streamweaver@gmail.com>
2017-08-26 00:32:57 -0400

    build from develop merge

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -76,15 +76,23 @@ AppComponent = __decorate([
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__components_navbar_navbar_component__ = __webpack_require__("../../../../../src/app/components/navbar/navbar.component.ts");
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__components_question_list_question_list_component__ = __webpack_require__("../../../../../src/app/components/question-list/question-list.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__components_navbar_navbar_component__ = __webpack_require__("../../../../../src/app/components/navbar/navbar.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__components_question_list_question_list_component__ = __webpack_require__("../../../../../src/app/components/question-list/question-list.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_6__services_data_service__ = __webpack_require__("../../../../../src/app/services/data.service.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_7__components_question_question_component__ = __webpack_require__("../../../../../src/app/components/question/question.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_8__components_add_question_add_question_component__ = __webpack_require__("../../../../../src/app/components/add-question/add-question.component.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
+
+
+
 
 
 
@@ -98,19 +106,95 @@ var AppModule = (function () {
 AppModule = __decorate([
     Object(__WEBPACK_IMPORTED_MODULE_1__angular_core__["L" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */],
-            __WEBPACK_IMPORTED_MODULE_3__components_navbar_navbar_component__["a" /* NavbarComponent */],
-            __WEBPACK_IMPORTED_MODULE_4__components_question_list_question_list_component__["a" /* QuestionListComponent */]
+            __WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_4__components_navbar_navbar_component__["a" /* NavbarComponent */],
+            __WEBPACK_IMPORTED_MODULE_5__components_question_list_question_list_component__["a" /* QuestionListComponent */],
+            __WEBPACK_IMPORTED_MODULE_7__components_question_question_component__["a" /* QuestionComponent */],
+            __WEBPACK_IMPORTED_MODULE_8__components_add_question_add_question_component__["a" /* AddQuestionComponent */]
         ],
         imports: [
-            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */]
         ],
-        providers: [],
-        bootstrap: [__WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]]
+        providers: [
+            __WEBPACK_IMPORTED_MODULE_6__services_data_service__["a" /* DataService */]
+        ],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_3__app_component__["a" /* AppComponent */]]
     })
 ], AppModule);
 
 //# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/add-question/add-question.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"card\">\n  <div class=\"card-header\">\n    Add New FAQ Entry\n  </div>\n  <div class=\"card-block\">\n    <form (submit)=\"addQuestion()\">\n      <div class=\"form-group\">\n        <label for=\"q\">Question</label>\n        <input class=\"form-control\" type=\"text\" required minlength=\"4\" [(ngModel)]=\"q\" name=\"q\" id=\"q\" #name=\"ngModel\">\n      </div>\n      <div class=\"form-group\">\n        <label for=\"a\">Answer</label>\n        <input class=\"form-control\" type=\"text\" [(ngModel)]=\"a\" name=\"a\" id=\"a\">\n      </div>\n      <input type=\"submit\" class=\"btn btn-primary\" value=\"Submit\">\n    </form>\n  </div>\n</div>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/add-question/add-question.component.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/add-question/add-question.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AddQuestionComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var AddQuestionComponent = (function () {
+    function AddQuestionComponent() {
+        this.questionAdded = new __WEBPACK_IMPORTED_MODULE_0__angular_core__["w" /* EventEmitter */]();
+    }
+    AddQuestionComponent.prototype.ngOnInit = function () {
+    };
+    AddQuestionComponent.prototype.addQuestion = function () {
+        this.questionAdded.emit({ q: this.q, a: this.a, hide: true });
+        this.q = null;
+        this.a = null;
+    };
+    return AddQuestionComponent;
+}());
+__decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["R" /* Output */])(),
+    __metadata("design:type", Object)
+], AddQuestionComponent.prototype, "questionAdded", void 0);
+AddQuestionComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-add-question',
+        template: __webpack_require__("../../../../../src/app/components/add-question/add-question.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/components/add-question/add-question.component.scss")]
+    }),
+    __metadata("design:paramtypes", [])
+], AddQuestionComponent);
+
+//# sourceMappingURL=add-question.component.js.map
 
 /***/ }),
 
@@ -178,7 +262,7 @@ NavbarComponent = __decorate([
 /***/ "../../../../../src/app/components/question-list/question-list.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"question\" *ngFor=\"let question of questions\">\n  <div class=\"card\">\n    <div class=\"card-header\">\n      {{ question.q }}\n    </div>\n    <div class=\"card-block\">\n      <p class=\"card-text\">{{ question.a }}</p>\n    </div>\n  </div>\n  <br>\n</div>\n"
+module.exports = "<app-add-question (questionAdded)=\"addQuestion($event)\"></app-add-question>\n<hr>\n<app-question *ngFor=\"let question of questions\" [question]=\"question\"></app-question>\n"
 
 /***/ }),
 
@@ -206,6 +290,7 @@ module.exports = module.exports.toString();
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return QuestionListComponent; });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__services_data_service__ = __webpack_require__("../../../../../src/app/services/data.service.ts");
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -216,24 +301,16 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 
+
 var QuestionListComponent = (function () {
-    function QuestionListComponent() {
-        this.questions = [
-            {
-                q: 'What is your name?',
-                a: 'My name is Scott'
-            },
-            {
-                q: 'What is your favorite color?',
-                a: 'My favorite color is blue'
-            },
-            {
-                q: 'What is your favorite programming language?',
-                a: 'My favorite programming language is python or Go'
-            }
-        ];
+    function QuestionListComponent(dataService) {
+        this.dataService = dataService;
     }
     QuestionListComponent.prototype.ngOnInit = function () {
+        this.questions = this.dataService.getQuestions();
+    };
+    QuestionListComponent.prototype.addQuestion = function (question) {
+        this.dataService.addQuestion(question);
     };
     return QuestionListComponent;
 }());
@@ -243,10 +320,157 @@ QuestionListComponent = __decorate([
         template: __webpack_require__("../../../../../src/app/components/question-list/question-list.component.html"),
         styles: [__webpack_require__("../../../../../src/app/components/question-list/question-list.component.scss")]
     }),
-    __metadata("design:paramtypes", [])
+    __metadata("design:paramtypes", [typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__services_data_service__["a" /* DataService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__services_data_service__["a" /* DataService */]) === "function" && _a || Object])
 ], QuestionListComponent);
 
+var _a;
 //# sourceMappingURL=question-list.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/question/question.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<div class=\"question\">\n  <div class=\"card\">\n    <div class=\"card-header\">\n      {{ question.q }}\n      <a href=\"#\" class=\"btn btn-link\" (click)=\"question.hide = !question.hide\">Show</a> |\n      <a href=\"#\" class=\"btn btn-link\" (click)=\"removeQuestion(question)\">Remove</a>\n    </div>\n    <div class=\"card-block\" [hidden]=\"question.hide\">\n      <p class=\"card-text\">{{ question.a }}</p>\n    </div>\n  </div>\n  <br>\n</div>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/question/question.component.scss":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/components/question/question.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return QuestionComponent; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__models_question__ = __webpack_require__("../../../../../src/app/models/question.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__models_question___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_1__models_question__);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__services_data_service__ = __webpack_require__("../../../../../src/app/services/data.service.ts");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+
+
+var QuestionComponent = (function () {
+    function QuestionComponent(dataService) {
+        this.dataService = dataService;
+    }
+    QuestionComponent.prototype.ngOnInit = function () {
+    };
+    QuestionComponent.prototype.removeQuestion = function (question) {
+        this.dataService.removeQuestion(question);
+    };
+    return QuestionComponent;
+}());
+__decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["E" /* Input */])('question'),
+    __metadata("design:type", typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__models_question__["Question"] !== "undefined" && __WEBPACK_IMPORTED_MODULE_1__models_question__["Question"]) === "function" && _a || Object)
+], QuestionComponent.prototype, "question", void 0);
+QuestionComponent = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-question',
+        template: __webpack_require__("../../../../../src/app/components/question/question.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/components/question/question.component.scss")]
+    }),
+    __metadata("design:paramtypes", [typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_2__services_data_service__["a" /* DataService */] !== "undefined" && __WEBPACK_IMPORTED_MODULE_2__services_data_service__["a" /* DataService */]) === "function" && _b || Object])
+], QuestionComponent);
+
+var _a, _b;
+//# sourceMappingURL=question.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/models/question.ts":
+/***/ (function(module, exports) {
+
+//# sourceMappingURL=question.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/services/data.service.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return DataService; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var DataService = (function () {
+    function DataService() {
+        this._dataFile = 'questions';
+        this.questions = this.getQuestions();
+    }
+    DataService.prototype.getQuestions = function () {
+        if (localStorage.getItem('questions') === null) {
+            this.questions = [];
+        }
+        else {
+            this.questions = JSON.parse(localStorage.getItem('questions'));
+        }
+        return this.questions;
+    };
+    DataService.prototype.addQuestion = function (question) {
+        this.questions.unshift(question);
+        this._storeQuestions();
+    };
+    DataService.prototype.removeQuestion = function (question) {
+        for (var i = 0; i < this.questions.length; i++) {
+            if (question === this.questions[i]) {
+                this.questions.splice(i, 1);
+                this._storeQuestions();
+            }
+        }
+    };
+    DataService.prototype._storeQuestions = function () {
+        localStorage.setItem(this._dataFile, JSON.stringify(this.questions));
+    };
+    DataService.prototype._loadQuestions = function () {
+        if (localStorage.getItem(this._dataFile) === null) {
+            this.questions = [];
+        }
+        else {
+            this.questions = JSON.parse(localStorage.getItem(this._dataFile));
+        }
+    };
+    return DataService;
+}());
+DataService = __decorate([
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["B" /* Injectable */])(),
+    __metadata("design:paramtypes", [])
+], DataService);
+
+//# sourceMappingURL=data.service.js.map
 
 /***/ }),
 
@@ -281,7 +505,7 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
-    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_19" /* enableProdMode */])();
+    Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_20" /* enableProdMode */])();
 }
 Object(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
 //# sourceMappingURL=main.js.map
