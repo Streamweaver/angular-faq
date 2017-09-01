4984aea1812192d31f4aef7acc3d6ff077af8fbc
Scott Turnbull <streamweaver@gmail.com>
2017-08-31 23:16:36 -0500

    Simple animation example

--- src/app/components/question-list/question-list.component.ts
+++ src/app/components/question-list/question-list.component.ts
@@ -1,26 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Question } from '../../models/question';
-import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
+
 
 @Component({
   selector: 'app-question-list',
   templateUrl: './question-list.component.html',
-  styleUrls: ['./question-list.component.scss'],
-  styles: [
-
-  ],
-  animations: [
-    trigger('growAnimation', [
-      state('small', style({
-        transform: 'scale(1)'
-      })),
-      state('large', style({
-        transform: 'scale(1.2)'
-      })),
-      transition('small => large', animate('300ms ease-in'))
-      ])
-  ]
+  styleUrls: ['./question-list.component.scss']
 })
 export class QuestionListComponent implements OnInit {
   questions: Question[];
--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,20 +1,42 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Question } from '../../models/question';
 import { DataService } from '../../services/data.service';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
-  styleUrls: ['./question.component.scss']
+  styleUrls: ['./question.component.scss'],  styles: [
+
+  ],
+  animations: [
+    trigger('growAnimation', [
+      state('small', style({
+        transform: 'scale(1)'
+      })),
+      state('large', style({
+        transform: 'scale(1.2)'
+      })),
+      transition('small => large', animate('300ms ease-in'))
+    ])
+  ]
 })
 export class QuestionComponent implements OnInit {
   @Input('question') question: Question;
+
+  growState: string;
+
   constructor(public dataService: DataService) { }
 
   ngOnInit() {
+    this.growState = 'small';
   }
 
   removeQuestion(question: Question) {
     this.dataService.removeQuestion(question);
+  }
+
+  animateGrow() {
+    this.growState = (this.growState === 'small' ? 'large' : 'small');
   }
 }
